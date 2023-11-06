a85c5e69d4c4cecd59d780f61fa70dea397500a7
Prathamesh Dhorage <prathameshdhorage@gmail.com>
2023-11-06 18:12:15 +0530

    working

--- customNER/model_server/main.py
+++ customNER/model_server/main.py
@@ -6,12 +6,14 @@ import spacy
 app = Flask(__name__)
 CORS(app)
 
-model_path = os.path.join(os.path.dirname(__file__), "../model/model-best")
+model_path = os.path.join(os.path.dirname(__file__), "../model/model-best/model-best")
 nlp_ner = spacy.load(model_path)
 
 def extract_entities(text):
     values = []
     cells = []
+    column = []
+    row = []
     type_labels = []
 
     doc = nlp_ner(text)
@@ -19,15 +21,21 @@ def extract_entities(text):
     for ent in doc.ents:
         if ent.label_ == "VALUE":
             values.append(ent.text)
-        elif ent.label_ in ("TYPE A", "TYPE B", "TYPE C"):
+        elif ent.label_ in ("INSERT", "DELETE", "REPLACE", "MERGE", "BOLD", "ITALIC"):
             type_labels.append(ent.label_)
         elif ent.label_ == "CELL":
             cells.append(ent.text)
+        elif ent.label_ == "COLUMN":
+            column.append(ent.text)
+        elif ent.label_ == "ROW":
+            row.append(ent.text)
 
     return {
         'values': values,
         'type_labels': type_labels,
-        'cells': cells
+        'cells': cells,
+        'column': column,
+        'row': row
     }
 
 @app.route('/predict', methods=['POST'])
